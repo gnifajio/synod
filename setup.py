a99325eec50a5c6fd2bc9bc881f2c05b26107679
gnifajio <gnifajio@gmail.com>
2022-06-02 16:37:42 +0300

    Setup fix

--- setup.py
+++ setup.py
@@ -3,21 +3,17 @@ import setuptools
 with open('README_EN.md', 'r') as fh:
 	long_description = fh.read()
 
-with open('requirements.txt', 'r') as fr:
-    requirements = map(lambda line: line.strip(), fr.readlines())
-
-
 setuptools.setup(
-	name = "gQiwiAPI",
-	version = "1.2",
+	name = "synod",
+	version = "1.0",
 	author = "Gnifajio None",
 	author_email = "gnifajio@gmail.com",
-	description = "A simple API for creating a payment link",
+	description = "Python dictionary with synonyms",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
-	url = "https://github.com/gnifajio/gQiwiAPI",
+	url = "https://github.com/gnifajio/synod",
 	packages = setuptools.find_packages(),
-	install_requires = requirements,
+	# install_requires = requirements,
 	classifiers = [
 		"Programming Language :: Python :: 3.10",
 		"License :: OSI Approved :: MIT License",
